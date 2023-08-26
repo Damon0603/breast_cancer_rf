e745a9dcc895d383e6d49d0684fa5e2f8d10ef5f
Sebastian <sebastiandamonbreuer@gmail.com>
2023-08-26 13:06:51 +0200

    done

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np 
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
